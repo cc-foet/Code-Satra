871209f691252225d64ff99737f4fe784f8656dc
Vishal Bist <131914809+Vishal-Bisht@users.noreply.github.com>
2024-07-06 12:07:45 +0530

    Day 4: Finding the index of the peak element in a mounted array

---
+++ Vishal bist/Peak_index.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int PeakIndex(vector<int>& nums, int start, int end) {
+       if(nums.size() == 1)
+            return 0;
+        int mid;
+        while(start < end) {
+            mid = start + (end - start)/2;
+            
+            if(nums[mid] > nums[mid + 1]) 
+                end = mid;
+            else 
+                start = mid + 1;
+        }
+        return start;
+    };
+
+int main(){
+  vector<int>nums = {1,9,11,16,18,29,34,77,96};
+  int result = PeakIndex(nums,0,nums.size()-1);
+  cout<<"index: "<<result<<endl<<"Element "<< nums[result]<<endl;
+}
