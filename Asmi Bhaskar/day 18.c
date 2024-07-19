c1223be3cd991dcf395f418f3f075ce0e9e080da
Asmi Bhaskar <blizzardeternal09@gmail.com>
2024-07-19 18:56:43 +0530

    Create day 18.c

---
+++ Asmi Bhaskar/day 18.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define INF 99999
+#define V 5
+
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V];
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+    
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+    
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V] = { 
+        {0, 4, INF, 5, INF},
+        {INF, 0, 1, INF, 6},
+        {2, INF, 0, 3, INF},
+        {INF, INF, 1, 0, 2},
+        {1, INF, INF, 4, 0}
+    };
+    
+    floydWarshall(graph);
+    return 0;
+}
