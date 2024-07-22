a27ad5e661b19e52dd47f761ab6a7cd7c1317cfe
sshikharr <shikhardwivedi2019@gmail.com>
2024-07-22 22:10:34 +0530

    Day 21

--- Shikhar/#DAY21 ReversingLinkedList.cpp
+++ Shikhar/#DAY21 ReversingLinkedList.cpp
@@ -33,3 +33,37 @@ public:
         return prev;
     }
 };
+
+// An optimized version of the code using recursion is as follows:
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ListNode* revRec(ListNode* temp, ListNode*prev){
+    if(temp==NULL){
+        return prev;
+    }
+    else{
+    ListNode* ahead = temp->next;
+    temp->next = prev;
+    prev = temp;
+    temp = ahead;
+    return revRec(temp, prev);
+    }
+}
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        ListNode* ans = revRec(temp, prev);
+        return ans;
+    }
+};
