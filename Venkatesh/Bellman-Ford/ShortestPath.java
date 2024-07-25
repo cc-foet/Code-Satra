9ef55122d39357dca4f05fd51748834f98ea0888
mherenow <venkatesh09072003@gmail.com>
2024-07-25 17:44:20 +0530

    update filename

--- Venkatesh/Bellman-Ford/ShortestPath.java
+++ Venkatesh/Bellman-Ford/ShortestPath.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Path {
+public class ShortestPath {
     public static Map<String, Integer> bellmanFord(Map<String, Map<String, Integer>> graph, String start) {
         Map<String, Integer> distances = new HashMap<>();
         Map<String, String> predecessors = new HashMap<>();
