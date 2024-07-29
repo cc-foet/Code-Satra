b04a513b6ea54122a0eda40282790f4675d8e958
shailjayadav30 <shailjayadav7275@gmail.com>
2024-07-29 20:37:20 +0530

    Day 27

---
+++ Shailja/Subsets.java
@@ -0,0 +1,25 @@
+    import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generateSubsets(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void generateSubsets(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            generateSubsets(i + 1, nums, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> subsets = subsets(nums);
+        System.out.println("Subsets: " + subsets);
+    }
+}
